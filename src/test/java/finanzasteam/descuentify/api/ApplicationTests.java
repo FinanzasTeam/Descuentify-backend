404c74de2abba6004578798498cd7cea0f6fa0e7
StevensAcha <u20201e735@upc.edu.pe>
2024-09-19 00:11:19 -0500

    first commit

---
+++ src/test/java/finanzasteam/descuentify/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package finanzasteam.descuentify.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
